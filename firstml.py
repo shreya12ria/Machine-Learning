3329ed6e6e6f8c8bf24b9974b597f8889e456bee
shreya12ria <shreya12ria@gmail.com>
2018-02-24 19:31:30 +0530

    Add files via upload

---
+++ firstml.py
@@ -0,0 +1,62 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello");
+
+
+
+
+# In[3]:
+
+
+import keras
+import tensorflow
+print("hello")
+
+
+# In[4]:
+
+
+import tensorflow
+print("hello")
+
+
+# In[5]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+numpy.random.seed(7)
+
+
+# In[12]:
+
+
+# Create your first MLP in Keras
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+# fix random seed for reproducibility
+numpy.random.seed(7)
+# load pima indians dataset
+dataset = numpy.loadtxt("ml\pimaindiansdiabetes.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, epochs=150, batch_size=10)
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
